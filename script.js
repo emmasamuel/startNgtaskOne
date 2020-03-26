505c56bb20c56f10eb10d43a0ca5a2a8dadfa3b1
Samuel <emmanuelstudentpage@gmail.com>
2020-03-26 21:23:31 +0100

    new added js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var name = 'samuel';
+var courses = ['html','css','js','c#'];
+//console.log(name,courses);
+var n = courses.length;
+//console.log(n);
+if(n % 2 === 0){
+for(var i = 0;i<200;i++){
+if(i % 2 === 0){
+console.log(courses[i]);
+}
+}
+}
